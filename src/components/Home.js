827f48c3428e331b779651311c270d53270808d4
AR <ruttlea@gmail.com>
2019-06-30 15:43:10 -0600

    wrap Home component with redux-react higher order component 'connect'

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,9 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import { Link } from 'react-router-dom';
 import Pokeball from '../pokeball.png';
+import { connect } from 'react-redux';
 
 class Home extends Component {
-  state = {
-    posts: []
-  };
-
-  componentDidMount() {
-    axios.get('https://jsonplaceholder.typicode.com/posts').then(res => {
-      console.log(res);
-      this.setState({
-        posts: res.data.slice(0, 30)
-      });
-    });
-  }
-
   render() {
     const { posts } = this.state;
     const postList = posts.length ? (
@@ -45,4 +32,4 @@ class Home extends Component {
   }
 }
 
-export default Home;
+export default connect()(Home);
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,5 +1,24 @@
 const initState = {
-  posts: []
+  posts: [
+    {
+      id: 1,
+      title: 'Squirtle Laid an Egg',
+      body:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem doloremque sed quibusdam sint ratione incidunt aut explicabo atque id. Quisquam est doloremque consequuntur accusantium, temporibus molestiae id vero exercitationem unde!'
+    },
+    {
+      id: 2,
+      title: 'Charmander Laid an Egg',
+      body:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem doloremque sed quibusdam sint ratione incidunt aut explicabo atque id. Quisquam est doloremque consequuntur accusantium, temporibus molestiae id vero exercitationem unde!'
+    },
+    {
+      id: 3,
+      title: 'A Helix Fossil was Found',
+      body:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem doloremque sed quibusdam sint ratione incidunt aut explicabo atque id. Quisquam est doloremque consequuntur accusantium, temporibus molestiae id vero exercitationem unde!'
+    }
+  ]
 };
 
 const rootReducer = (state = initState, action) => {
