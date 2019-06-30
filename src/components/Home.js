d461221673ca3efe6b7f495a9350d7e02c90c6a5
AR <ruttlea@gmail.com>
2019-06-30 13:58:16 -0600

    pull in data from https://jsonplaceholder.typicode.com/posts

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Home = () => {
-  return (
-    <div className="container">
-      <h4 className="center">Home</h4>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente praesentium unde quidem facilis culpa qui
-        accusantium quae fugit numquam pariatur iure eius, ab ipsam eaque similique dolores, alias minima harum?
-      </p>
-    </div>
-  );
-};
+class Home extends Component {
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
+      console.log(response);
+    });
+  }
+  render() {
+    return (
+      <div className="container">
+        <h4 className="center">Home</h4>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente praesentium unde quidem facilis culpa qui
+          accusantium quae fugit numquam pariatur iure eius, ab ipsam eaque similique dolores, alias minima harum?
+        </p>
+      </div>
+    );
+  }
+}
 
 export default Home;
