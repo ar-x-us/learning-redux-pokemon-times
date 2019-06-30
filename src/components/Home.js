efbf8fafe89232bc3a335cf50e55ab3e8d0508ae
AR <ruttlea@gmail.com>
2019-06-30 14:02:49 -0600

    render data obtained from https://jsonplaceholder.typicode.com/posts

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,19 +2,39 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class Home extends Component {
+  state = {
+    posts: []
+  };
+
   componentDidMount() {
-    axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
-      console.log(response);
+    axios.get('https://jsonplaceholder.typicode.com/posts').then(res => {
+      console.log(res);
+      this.setState({
+        posts: res.data.slice(0, 30)
+      });
     });
   }
+
   render() {
+    const { posts } = this.state;
+    const postList = posts.length ? (
+      posts.map(post => {
+        return (
+          <div className="post card" key={post.id}>
+            <div className="card-content">
+              <span className="card-title">{post.title}</span>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        );
+      })
+    ) : (
+      <div className="center">No posts yellowBright.</div>
+    );
     return (
       <div className="container">
         <h4 className="center">Home</h4>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente praesentium unde quidem facilis culpa qui
-          accusantium quae fugit numquam pariatur iure eius, ab ipsam eaque similique dolores, alias minima harum?
-        </p>
+        <div>{postList}</div>
       </div>
     );
   }
