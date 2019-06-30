ec0a5caecb122a8cad51e99d3a6370a769f53b22
AR <ruttlea@gmail.com>
2019-06-30 14:21:26 -0600

    render post selected from Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
   state = {
@@ -21,10 +22,12 @@ class Home extends Component {
       posts.map(post => {
         return (
           <div className="post card" key={post.id}>
-            <div className="card-content">
-              <span className="card-title">{post.title}</span>
-              <p>{post.body}</p>
-            </div>
+            <Link to={'/' + post.id}>
+              <div className="card-content">
+                <span className="card-title">{post.title}</span>
+                <p>{post.body}</p>
+              </div>
+            </Link>
           </div>
         );
       })
