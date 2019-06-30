d13f87b972fe2cb835e7a36920ccbe8fe2d20f2e
AR <ruttlea@gmail.com>
2019-06-30 13:47:27 -0600

    create higher order component: Rainbow

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Rainbow from '../hoc/Rainbow';
 
 const About = () => {
   return (
@@ -12,4 +13,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default Rainbow(About);
